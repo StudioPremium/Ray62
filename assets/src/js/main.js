42e91302b0e1c405a4b8d4eb3b16d3dd0cc6b936
MaksimLavrenyuk <maksim.lavrenyuk@outlook.com>
2019-02-07 12:41:30 +0400

    Added autocomplete

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -19,7 +19,15 @@
 // Импортируем телефонную валидацию
 //= ../../../node_modules/jquery.maskedinput/src/jquery.maskedinput.js
 
-
+// Импортируем jquery-ui
+//= ../../../node_modules/jquery-ui/ui/widget.js
+//= ../../../node_modules/jquery-ui/ui/widgets/autocomplete.js
+//= ../../../node_modules/jquery-ui/ui/widgets/menu.js
+//= ../../../node_modules/jquery-ui/ui/keycode.js
+//= ../../../node_modules/jquery-ui/ui/position.js
+//= ../../../node_modules/jquery-ui/ui/safe-active-element.js
+//= ../../../node_modules/jquery-ui/ui/version.js
+//= ../../../node_modules/jquery-ui/ui/unique-id.js
 
 // Импортируем другие js-файлы
 
